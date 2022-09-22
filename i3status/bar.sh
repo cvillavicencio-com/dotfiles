b2d473ed14cd60dc9dac6cc103e745f76d8bf9fa
Camilo Villavicencio <laboral@cvillavicencio.com>
2022-09-22 03:00:53 -0300

    i3 + i3status

---
+++ i3status/bar.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+i3status | while :
+do
+    read line
+    luz=$(light)
+    mails1=$(ls ~/Mail/laboral/INBOX/new/ | wc -l)
+    mails2=$(ls ~/Mail/laboral/INBOX/cur/*, | wc -l)
+    ventana=$(xprop -id $(xprop -root _NET_ACTIVE_WINDOW | cut -d ' ' -f 5) WM_NAME | sed -e 's/.*"\(.*\)".*/\1/';)
+    echo "$mails1/$mails2 - $ventana  | ☀️: $luz | $line" || exit 1
+done
